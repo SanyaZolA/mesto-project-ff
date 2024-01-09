3be94ddeb1f3a60fbb1cc87379eef0f7f70e564d
Alexandr Sergeev <ntl1221@yandex.ru>
2024-01-10 00:25:41 +0300

    done

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -4,16 +4,16 @@ export const initialCards = [
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      name: "Оберстдорф, Германия",
+      link: "https://images.unsplash.com/photo-1688455524398-09e385936a09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      name: "Сен-Жерве-ле-Бен, Франция",
+      link: "https://images.unsplash.com/photo-1606730004798-e77a54f5e46e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
     },
     {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      name: "Кефалиния, Греция",
+      link: "https://images.unsplash.com/photo-1631992339428-8ab5567b0eed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
     },
     {
       name: "Холмогорский район",
---
+++ src/scripts/index.js
@@ -0,0 +1,64 @@
+import { initialCards } from './cards.js';
+import '../pages/index.css';
+import { cardTemplate, placesList, popupZoom, openZoomPopup, createNewPost, initializeList, imageZoom, signatureZoom } from './card.js'; 
+import { openPopup, closePopup, closePopupEsc, closePopupOverlay } from "./modal.js";
+
+const content = document.querySelector('.content');
+const popupProfile = document.querySelector('.popup_type_edit');  // присваивание элементов для попап профиля.
+const popupAddFoto = document.querySelector('.popup_type_new-card');  // присваивание элементов для попап картинок.
+
+// //----------------------------------НАХОЖДЕНИЕ КНОПОК---------------------------------------------------------------------------
+const buttonEditPopupProfile = content.querySelector('.profile__edit-button'); // открытие редактирования профиля.
+const buttonOpenPopupAddFoto = content.querySelector('.profile__add-button');  // открытие добавления картинки.
+const closeButtons = document.querySelectorAll('.popup__close-button');        // вешаем закрытие крестик на переменную
+
+// //----------------------------------НАХОЖДЕНИЕ ИНПУТОВ ДЛЯ ВВОДА ИНФОРМАЦИИ-----------------------------------------------------
+const inputNameProfile = popupProfile.querySelector('.popup__input_type_name');          // ввод имени в попап профиля.
+const inputAboutProfile = popupProfile.querySelector('.popup__input_type_description');  // ввод 'о себе' в попап профиля.
+const inputNameCard = popupAddFoto.querySelector('.popup__input_type_card-name');        // ввод название в попап картинок.
+const inputUrlFoto = popupAddFoto.querySelector('.popup__input_type_url');               // ввод ссылки в попап картинок.
+
+// //----------------------------------НАХОЖДЕНИЕ ПОЛЕЙ ВВОДА ДЛЯ РЕДАКТИРОВАНИЯ---------------------------------------------------
+const profileName = content.querySelector('.profile__title');        // ввод имени в попап профиля.
+const profileWho = content.querySelector('.profile__description');   // ввод 'о себе' в попап профиля.
+
+// //----------------------------------НАХОЖДЕНИЕ ФОРМ ВВОДА-----------------------------------------------------------------------
+const formEditProfile = popupProfile.querySelector('.popup__form');  // форма ввода редактирования профиля.
+const formAddFoto = popupAddFoto.querySelector('.popup__form');      // форма ввода добавление картинок.
+
+// //----------------------------------ОТКРЫТИЕ ПОПАПОВ ПРОФИЛЯ И КАРТИНОК------------------------------------------------
+buttonEditPopupProfile.addEventListener('click', openPopupProfile);                       // вешаем событие(открыть) редактирование профиля на клик мыши.
+buttonOpenPopupAddFoto.addEventListener('click', () => openPopup(popupAddFoto));          // вешаем событие(открыть) добавление картинок на клик мыши.
+
+initializeList(initialCards);    // сама инициализация предустановленных картинок. Начинается формироваться список при загрузке страницы      
+
+
+closeButtons.forEach((button) => {                                                        // автоматическая обработка кнопок закрытия 'крест '
+  const popup = button.closest('.popup');
+  button.addEventListener('click', () => closePopup(popup));
+});
+
+function openPopupProfile () {                                                            // функция открытия заполненого профиля.
+  inputNameProfile.value = profileName.textContent;                                       // что в html добавляется в имя
+  inputAboutProfile.value = profileWho.textContent;                                       // что в html добавляется в профессия
+  openPopup(popupProfile);
+}
+
+formAddFoto.addEventListener('submit', handleAddFormSubmit);                     // вешаем событие(закрыть) на событие 'sudmit' - отправка фото в список
+
+function handleAddFormSubmit(evt) {                                              // заполненная форма добавления фото, встаёт в список.
+  evt.preventDefault();                                                          // сброс стандартной отправки, перезагрузки картинок.
+  const newElement = createNewPost(inputNameCard.value, inputUrlFoto.value);     // берём то что заполнили в форме и добавляем в переменную.
+  placesList.prepend(newElement);                                                // новый пост идёт в начало листа.
+  closePopup(popupAddFoto)                                                       // делает функцию для закрытия.
+  evt.target.reset()                                                             // сбрасывает форму.
+};
+
+// //----------------------------------СОХРАНЕНИЕ ИНФОРМАЦИИ ПРИ РЕДАКТИРОВАНИИ ПРОФИЛЯ--------------------------------------------
+function saveProfilePopup (evt) {
+  evt.preventDefault();                                          // отмена стандартной формы отправки.
+  profileName.textContent = inputNameProfile.value;              // сохранение введенной информации ФИО и замена в профиле.
+  profileWho.textContent = inputAboutProfile.value;              // сохранение введенной информации О СЕБЕ и замена в профиле.
+  closePopup(popupProfile);      
+};
+formEditProfile.addEventListener('submit', saveProfilePopup);    // на форму вешаем событие(сохранить данные) при 'оптравить'.
